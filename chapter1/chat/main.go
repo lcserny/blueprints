cdc134418d2679bc64b464b7f3f34a2ad340e927
sabyx <sabyx2006@yahoo.com>
2018-08-10 16:25:52 +0300

    p28: tracing code to get a look under the hood

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+	"flag"
 )
 
 // represents a single template
@@ -20,13 +21,23 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
 	// start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting the web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAdnServer:", err)
 	}
 }
---
+++ chapter1/chat/room.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: messageBufferSize}
+
+type room struct {
+	// forward is a channnel that holds incomming messages
+	// that should be forwarded to the other clients
+	forward chan []byte
+	// join is a channel for clients wishing to join the room
+	join chan *client
+	// leave is a channel for clients wishing to leave the room
+	leave chan *client
+	// clients holds all current clients in this room
+	clients map[*client]bool
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
+
+func (r *room) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	socket, err := upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
